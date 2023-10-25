d034cfdbebca3abbed4627f7f8fe98195958b791
enurv <var.eda.nur@gmail.com>
2023-10-25 03:44:15 +0300

    feat(dto): create ItemDTO class

---
+++ src/main/java/com/example/cartservice/dto/ItemDTO.java
@@ -0,0 +1,9 @@
+package com.example.cartservice.dto;
+
+public class ItemDTO {
+    private int itemId;
+    private int categoryId;
+    private int sellerId;
+    private double price;
+    private int quantity;
+}
