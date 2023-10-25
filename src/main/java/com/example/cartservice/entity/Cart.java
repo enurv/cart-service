54faaee4127f69d2385a1646601074d8a3ed9607
enurv <var.eda.nur@gmail.com>
2023-10-25 03:04:46 +0300

    feat(cart): create properties and methods

--- src/main/java/com/example/cartservice/entity/Cart.java
+++ src/main/java/com/example/cartservice/entity/Cart.java
@@ -1,5 +1,7 @@
 package com.example.cartservice.entity;
 
+import com.example.cartservice.entity.item.Item;
+import com.example.cartservice.entity.promotion.Promotion;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -7,9 +9,31 @@ import java.util.UUID;
 
 @Component
 public class Cart {
+    private final int MAX_ITEM_NUMBER = 30;
+    private final int MAX_UNIQUE_ITEM_NUMBER = 10;
+    private final int MAX_TOTAL_PRICE = 500000;
+
     private UUID id;
     private double totalPrice;
     private Promotion promotion;
     private int itemCount;
     private List<Item> items;
+
+    public void addItem(List<Item> items) {}
+
+    public void removeItem(List<Item> items) {}
+
+    public List<Item> getCartItems() {
+        return null;
+    }
+
+    public int getItemCount() {
+        return 0;
+    }
+
+    public double getTotalPrice() {
+        return 0;
+    }
+
+    private void calculateTotalPrice() {}
 }
