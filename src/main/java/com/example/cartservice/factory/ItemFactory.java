9b23b386f1e83e6a780f362b174add7584f3df92
enurv <var.eda.nur@gmail.com>
2023-10-26 01:33:04 +0300

    refactor(factory): move ItemFactory into separate package

---
+++ src/main/java/com/example/cartservice/factory/ItemFactory.java
@@ -0,0 +1,13 @@
+package com.example.cartservice.factory;
+
+import com.example.cartservice.dto.ItemDTO;
+import com.example.cartservice.entity.item.Item;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public class ItemFactory {
+    public List<Item> createItems(ItemDTO itemDto) {
+        return null;
+    }
+}
