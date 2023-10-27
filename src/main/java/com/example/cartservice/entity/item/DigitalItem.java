b3e2736036080ad71d9c03a1dcff2b01575193a1
enurv <var.eda.nur@gmail.com>
2023-10-27 03:38:21 +0300

    refactor(item): utilize constants

--- src/main/java/com/example/cartservice/entity/item/DigitalItem.java
+++ src/main/java/com/example/cartservice/entity/item/DigitalItem.java
@@ -1,10 +1,12 @@
 package com.example.cartservice.entity.item;
 
+import static com.example.cartservice.Constants.DIGITAL_ITEM_CATEGORY_ID;
+
 public class DigitalItem extends Item {
 
     public DigitalItem(int id, double price, int categoryId, int sellerId, int quantity) {
         super(id, price, categoryId, sellerId, quantity);
         this.maxQuantity = 5;
-        this.categoryId = 7889;
+        this.categoryId = DIGITAL_ITEM_CATEGORY_ID;
     }
 }
--- src/main/java/com/example/cartservice/factory/ItemFactory.java
+++ src/main/java/com/example/cartservice/factory/ItemFactory.java
@@ -5,10 +5,14 @@ import com.example.cartservice.entity.item.DefaultItem;
 import com.example.cartservice.entity.item.DigitalItem;
 import com.example.cartservice.entity.item.Item;
 import com.example.cartservice.entity.item.VasItem;
+import org.springframework.stereotype.Component;
 
+import static com.example.cartservice.Constants.DIGITAL_ITEM_CATEGORY_ID;
+
+@Component
 public class ItemFactory {
     public Item createItem(ItemDTO itemDto) {
-        if (itemDto.categoryId == 7889) {
+        if (itemDto.categoryId == DIGITAL_ITEM_CATEGORY_ID) {
             return new DigitalItem(itemDto.itemId, itemDto.price, itemDto.categoryId, itemDto.sellerId, itemDto.quantity);
         } else {
             return new DefaultItem(itemDto.itemId, itemDto.price, itemDto.categoryId, itemDto.sellerId, itemDto.quantity);
