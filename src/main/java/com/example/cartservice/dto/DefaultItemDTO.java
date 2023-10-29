a65da8849e0291e06b491e9f5debf20c428a0913
enurv <var.eda.nur@gmail.com>
2023-10-30 00:17:43 +0300

    fix(dto): change vasItemList property name

--- src/main/java/com/example/cartservice/dto/DefaultItemDTO.java
+++ src/main/java/com/example/cartservice/dto/DefaultItemDTO.java
@@ -6,15 +6,15 @@ import com.example.cartservice.entity.item.VasItem;
 import java.util.List;
 
 public class DefaultItemDTO extends ItemDTO {
-    public List<VasItemDTO> vasItemDTOs;
+    public List<VasItemDTO> vasItems;
 
     public DefaultItemDTO(int itemId, int categoryId, int sellerId, double price, int quantity, List<VasItemDTO> vasItems) {
         super(itemId, categoryId, sellerId, price, quantity);
-        this.vasItemDTOs = vasItems;
+        this.vasItems = vasItems;
     }
 
     public DefaultItemDTO(DefaultItem defaultItem) {
         super(defaultItem.getId(), defaultItem.getCategoryId(), defaultItem.getSellerId(), defaultItem.getPrice(), defaultItem.getQuantity());
-        this.vasItemDTOs = defaultItem.getVasItems().stream().map(VasItemDTO::new).toList();
+        this.vasItems = defaultItem.getVasItems().stream().map(VasItemDTO::new).toList();
     }
 }
